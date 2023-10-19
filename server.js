2a521c4a4847f59746e2e0e631dc4eeeeda5f10f
Paulo09 <paullocasttro@hotmail.com>
2023-10-18 21:53:17 -0300

    Update Todos os usuarios

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ import Connection from "./config/Connection.js";
 
 const app = express();
 
+app.use(express.json());
+
 
 app.get('/teste1', function(request,response){
      
@@ -72,6 +74,37 @@ app.get('/usuarios/:id', function(request, response) {
         response.json(results);
     })
 });
+
+
+
+app.post('/usuarios', function(request, response) {
+    const body = request.body;
+    const connect = new Connection();
+    connect.query("INSERT INTO usuarios SET ?", body, function(error, results) {
+        response.json(results);
+    })
+});
+
+app.put('/usuarios/:id', function(request, response) {
+    const id = request.params.id;
+    const body = request.body;
+    const connect = new Connection();
+    connect.query("UPDATE usuarios SET ? WHERE id = ?", [body, id], function(error, results) {
+        response.json(results);
+    })
+});
+
+
+app.delete('/usuarios/:id', function(request, response) {
+    const id = request.params.id;
+    const connect = new Connection();
+    connect.query("DELETE FROM usuarios WHERE id = ?", [id], function(error, results) {
+        response.json(results);
+    })
+});
+
+
+
 
 app.put('/usuarios/:id', function(request, response) {
     const id = request.params.id;
